0557d855fd5d439896e575f3aaf9b799f05cf3e7
Phishsaw <phishsaw@gmail.com>
2015-12-25 21:04:34 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Write the following functions:
+##makeCacheMatrix: This function creates a special "matrix" 
+##object that can cache its inverse.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(x = matrix()){
+	inv <- NULL
+	set <- function(y) { 						##defines a function to set the matrix 
+                x <<- y							##to a new matrix , y, and resets the inverse, inv, to NULL
+                inv <<- NULL
+        }
+        get <- function() x  					        ##returns the matrix, x
+        setinverse <- function(solve) inv <<- solve	                ##sets the inverse, inv, to solve
+        getinverse <- function() inv 				        ##returns the inverse, inv
+        list(set = set, get = get, setinverse = setinverse,  getinverse = getinverse)##returns the 'special matrix'
+	
 }
 
 
-## Write a short comment describing this function
+##cacheSolve: This function computes the inverse of the 
+##special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has already been calculated (and the matrix 
+##has not changed), then the cachesolve should retrieve the 
+##inverse from the cache.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()				##checks for cached inverse 
+        if(!is.null(inv)) {				##if inverse exists then get cached data
+                message("getting cached data")	        ##Prints "getting cached data"
+                return(inv)				##return the cached inverse data
+        }
+	else{					        ##if cached data doesn't exist
+        data <- x$get()					##get current matrix data
+        inv <- solve(data, ...)				##perform the inverse for the matrix			
+        x$setinverse(inv)				##set the inverse data in cache
+	   }
+	   return(inv)					##return the inverse matrix
 }
